f82bd5af268c3ea4822ba9685d1f93b69e5d778b
Adarsh Vinod <adarshvinod12@gmail.com>
2024-11-06 01:14:50 +0530

    Added update and delete functionality

--- Jotter/Jotter/obj/Debug/net8.0/Jotter.AssemblyInfo.cs
+++ Jotter/Jotter/obj/Debug/net8.0/Jotter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jotter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012760d931fff47d4539fa9850d2420b55af27a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jotter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jotter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
